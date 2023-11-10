dcaf67c8c7dd766d30d72ccbc0af7b2e141bdeab
lmarschall <lmarschall.com@gmail.com>
2023-11-10 11:25:58 +0000

    further cleaning up the code

--- backend/Dockerfile
+++ backend/Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16
+FROM node:18
 
 # install simple http server for serving static content
 RUN npm install -g http-server
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,28 +5,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 
-import report = require('./report');
-import application = require('./application');
-import serviceauthn = require('./serviceauthn');
 import webauthn = require('./webauthn');
 import redis = require('./redis');
-// import mail = require('./mail');
 
 const port = process.env.PORT || 8080;
 
 redis.initRedis();
-// mail.sendMail();
 
 const app = express();
 
-// const medcastUrlRegExp = /(http(s)?:\/\/)(.foodover.app)|(localhost)/;
-// const corsOptions = {
-//     origin: medcastUrlRegExp
-// };
-
-// app.use(cors(corsOptions));
-
-const whitelist = ['https://localhost:3000', 'https://pwahub.one'];
+const whitelist = ['https://localhost:3000'];
 
 const corsOptionsDelegate = (req, callback) => {
 
@@ -47,13 +35,9 @@ const corsOptionsDelegate = (req, callback) => {
 }
 
 app.use(cors(corsOptionsDelegate));
-// app.use(cors());
 app.use(helmet());
 app.use(compression()); // COMPRESSION
 app.use(bodyParser.json({limit: '1mb'}));
-app.use('/report', report.router);
-app.use('/application', application.router);
-app.use('/serviceauthn', serviceauthn.router);
 app.use('/webauthn', webauthn.router);
 
 app.listen(port, () => {
