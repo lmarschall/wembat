85f22a9ea3b827c2856bdc0db3b67af3e6b509b5
lmarschall <lmarschall.com@gmail.com>
2024-01-27 18:52:19 +0100

    created database migrations

---
+++ backend/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash+
+
+# deploy migrations to database
+npx prisma migrate deploy
+
+# start api server
+npm run start
---
+++ backend/prisma/migrations/20240127174743_init/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "uid" TEXT NOT NULL,
+    "challenge" TEXT NOT NULL DEFAULT E'',
+    "mail" TEXT NOT NULL,
+    "token" TEXT NOT NULL DEFAULT E'',
+    "publicKey" TEXT NOT NULL DEFAULT E'',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("uid")
+);
+
+-- CreateTable
+CREATE TABLE "Device" (
+    "id" SERIAL NOT NULL,
+    "userUId" TEXT NOT NULL,
+    "credentialPublicKey" BYTEA NOT NULL,
+    "credentialId" BYTEA NOT NULL,
+    "counter" INTEGER NOT NULL,
+    "written" BOOLEAN NOT NULL DEFAULT false,
+    "transports" TEXT[],
+
+    CONSTRAINT "Device_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_challenge_key" ON "User"("challenge");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_mail_key" ON "User"("mail");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Device_credentialId_key" ON "Device"("credentialId");
+
+-- AddForeignKey
+ALTER TABLE "Device" ADD CONSTRAINT "Device_userUId_fkey" FOREIGN KEY ("userUId") REFERENCES "User"("uid") ON DELETE RESTRICT ON UPDATE CASCADE;
