d907752ed25af517af69d2f2338aedc352ad20e6
lmarschall <lmarschall.com@gmail.com>
2024-08-04 12:05:32 +0200

    more docs

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,9 @@
+/**
+ * Converts a string to an ArrayBuffer.
+ * 
+ * @param str - The string to convert.
+ * @returns The converted ArrayBuffer.
+ */
 export function str2ab(str: string): ArrayBuffer {
     str = atob(str);
     const buf = new ArrayBuffer(str.length);
@@ -8,10 +14,23 @@ export function str2ab(str: string): ArrayBuffer {
     return buf;
 }
 
+/**
+ * Converts an ArrayBuffer to a string using base64 encoding.
+ * 
+ * @param buf - The ArrayBuffer to convert.
+ * @returns The base64 encoded string.
+ */
 export function ab2str(buf: ArrayBuffer): string {
     return btoa(String.fromCharCode.apply(null, [...new Uint8Array(buf)]));
 }
 
+/**
+ * Derives an encryption key using the provided private and public keys.
+ * @param privateKey - The private key used for key derivation.
+ * @param publicKey - The public key used for key derivation.
+ * @returns A promise that resolves to the derived encryption key.
+ * @throws An error if the encryption key could not be derived.
+ */
 export async function deriveEncryptionKey(privateKey: CryptoKey, publicKey: CryptoKey): Promise<CryptoKey> {
     if (privateKey !== undefined && publicKey !== undefined) {
         const encryptionKey = await window.crypto.subtle.deriveKey(
@@ -33,11 +52,22 @@ export async function deriveEncryptionKey(privateKey: CryptoKey, publicKey: Cryp
     }
 }
 
+/**
+ * Saves a CryptoKey as a string by exporting it in JWK format.
+ * @param cryptoKey The CryptoKey to be saved.
+ * @returns A Promise that resolves to the exported CryptoKey as a string.
+ */
 export async function saveCryptoKeyAsString(cryptoKey: CryptoKey): Promise<string> {
     const exported = await window.crypto.subtle.exportKey("jwk", cryptoKey);
     return JSON.stringify(exported);
 }
 
+/**
+ * Loads a cryptographic public key from a string representation.
+ * @param pubKeyString The string representation of the public key.
+ * @returns A promise that resolves to the loaded CryptoKey object.
+ * @throws {Error} If the public key string is empty.
+ */
 export async function loadCryptoPublicKeyFromString(
     pubKeyString: string
 ): Promise<CryptoKey> {
@@ -57,6 +87,12 @@ export async function loadCryptoPublicKeyFromString(
     }
 }
 
+/**
+ * Loads a cryptographic private key from a string representation.
+ * @param privateKeyString The string representation of the private key.
+ * @returns A promise that resolves to the loaded CryptoKey object.
+ * @throws {Error} If the private key string is empty.
+ */
 export async function loadCryptoPrivateKeyFromString(
     privateKeyString: string
 ): Promise<CryptoKey> {
