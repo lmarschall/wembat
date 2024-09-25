fdecede421ca1919ca6619af66a496f95580fe63
lmarschall <lmarschall.com@gmail.com>
2024-09-26 01:40:08 +0200

    basic dashboard layout

--- backend/dashboard/src/main.ts
+++ backend/dashboard/src/main.ts
@@ -3,6 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
 import App from './App.vue'
 import router from './router'
 
---
+++ backend/dashboard/src/views/DashboardView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="d-flex flex-column flex-lg-row h-100">
+    <div class="flex-shrink-1">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary h-100 d-flex flex-row flex-lg-column">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">Offcanvas navbar</a>
+        <div></div>
+        <div class="offcanvas offcanvas-start h-100" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+          <ul class="nav d-flex flex-column h-100">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="flex-grow-1">
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <div class="flex-grow-1">
+      BLOB
+    </div>
+  </div>
+</template>
