72df02fe06f6ef2d0b8b6e16928aadd2cb22a5bc
lmarschall <lmarschall.com@gmail.com>
2024-09-27 23:51:29 +0200

    create add button

--- backend/dashboard/src/components/ApplicationsItem.vue
+++ backend/dashboard/src/components/ApplicationsItem.vue
@@ -8,7 +8,11 @@ onMounted(() => {
   console.log('mounted');
 
   let table = new DataTable('#myTable', {
-    responsive: true
+    responsive: true,
+    info: false,
+    ordering: true,
+    paging: false,
+    searching: false,
   });
 })
 
@@ -31,8 +35,32 @@ defineProps<{
     {{ applicationModel.status }}
     {{ applicationModel.date }}
 
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <table id="myTable" class="table">
-      <thead>
+      <thead class="table-dark">
         <tr>
           <th scope="col">#</th>
           <th scope="col">First</th>
