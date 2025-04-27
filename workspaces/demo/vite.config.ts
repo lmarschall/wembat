877e5d6851e4143a9b50e657693e11e17193c367
lmarschall <lmarschall.com@gmail.com>
2025-04-27 09:10:13 +0200

    some minot house keeping

--- workspaces/demo/vite.config.ts
+++ workspaces/demo/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), basicSsl()],
-  server: {
-    https: true,
-  },
+	plugins: [vue(), basicSsl()],
+	server: {
+		https: true,
+	},
 });
